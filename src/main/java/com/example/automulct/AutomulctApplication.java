f79715f707be0ea02da85a2f7cc82fe07c6b9e7c
Stanislav Nemirovsky <choonsky@gmail.com>
2019-06-12 17:53:14 +0700

    IDEA initial

---
+++ src/main/java/com/example/automulct/AutomulctApplication.java
@@ -0,0 +1,13 @@
+package com.example.automulct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomulctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomulctApplication.class, args);
+    }
+
+}
